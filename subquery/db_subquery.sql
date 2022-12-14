9df336f698ae72913d71cec63f2a161b48bd71eb
LeeYeongin <youngin5757@gmail.com>
2022-12-14 12:22:30 +0900

    db_subquery 데이터백업

--- subquery/db_subquery.sql
+++ subquery/db_subquery.sql
@@ -1,91 +1,177 @@
--- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        8.0.31 - MySQL Community Server - GPL
--- 서버 OS:                        Win64
--- HeidiSQL 버전:                  12.3.0.6589
--- --------------------------------------------------------
+CREATE DATABASE  IF NOT EXISTS `subquery_study` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `subquery_study`;
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: subquery_study
+-- ------------------------------------------------------
+-- Server version	8.0.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `board_mst`
+--
 
--- subquery_study 데이터베이스 구조 내보내기
-DROP DATABASE IF EXISTS `subquery_study`;
-CREATE DATABASE IF NOT EXISTS `subquery_study` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `subquery_study`;
-
--- 테이블 subquery_study.board_mst 구조 내보내기
 DROP TABLE IF EXISTS `board_mst`;
-CREATE TABLE IF NOT EXISTS `board_mst` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `board_mst` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `content` text COLLATE utf8mb4_general_ci NOT NULL,
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `read_count` int NOT NULL DEFAULT '0',
   `writer_id` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- 테이블 데이터 subquery_study.board_mst:~0 rows (대략적) 내보내기
-DELETE FROM `board_mst`;
-INSERT INTO `board_mst` (`id`, `title`, `content`, `read_count`, `writer_id`) VALUES
-	(1, '제목1', '게시글 내용1', 0, 1),
-	(2, '제목2', '게시글 내용2', 0, 1),
-	(3, '제목3', '게시글 내용3', 0, 1),
-	(4, '제목1', '게시글 내용1', 0, 2),
-	(5, '제목2', '게시글 내용2', 0, 2),
-	(6, '제목3', '게시글 내용3', 0, 2),
-	(7, '제목4', '게시글 내용4', 0, 1),
-	(8, '제목5', '게시글 내용5', 0, 1),
-	(9, '제목6', '게시글 내용6', 0, 1),
-	(10, '제목4', '게시글 내용4', 0, 2),
-	(11, '제목5', '게시글 내용5', 0, 2);
+--
+-- Dumping data for table `board_mst`
+--
 
--- 테이블 subquery_study.user_dtl 구조 내보내기
+LOCK TABLES `board_mst` WRITE;
+/*!40000 ALTER TABLE `board_mst` DISABLE KEYS */;
+INSERT INTO `board_mst` VALUES (1,'제목1','게시글 내용1',0,1),(2,'제목2','게시글 내용2',0,1),(3,'제목3','게시글 내용3',0,1),(4,'제목1','게시글 내용1',0,2),(5,'제목2','게시글 내용2',0,2),(6,'제목3','게시글 내용3',0,2),(7,'제목4','게시글 내용4',0,1),(8,'제목5','게시글 내용5',0,1),(9,'제목6','게시글 내용6',0,1),(10,'제목4','게시글 내용4',0,2),(11,'제목5','게시글 내용5',0,2);
+/*!40000 ALTER TABLE `board_mst` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `score_mst`
+--
+
+DROP TABLE IF EXISTS `score_mst`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `score_mst` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `score` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `score_mst`
+--
+
+LOCK TABLES `score_mst` WRITE;
+/*!40000 ALTER TABLE `score_mst` DISABLE KEYS */;
+INSERT INTO `score_mst` VALUES (1,'신경수',100),(2,'고희주',95),(3,'장건녕',85),(4,'문승주',80),(5,'이승아',75),(6,'김수현',65),(7,'문경원',50),(8,'이일인',-10);
+/*!40000 ALTER TABLE `score_mst` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_dtl`
+--
+
 DROP TABLE IF EXISTS `user_dtl`;
-CREATE TABLE IF NOT EXISTS `user_dtl` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_dtl` (
   `id` int NOT NULL,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `phone` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- 테이블 데이터 subquery_study.user_dtl:~0 rows (대략적) 내보내기
-DELETE FROM `user_dtl`;
+--
+-- Dumping data for table `user_dtl`
+--
 
--- 테이블 subquery_study.user_mst 구조 내보내기
+LOCK TABLES `user_dtl` WRITE;
+/*!40000 ALTER TABLE `user_dtl` DISABLE KEYS */;
+INSERT INTO `user_dtl` VALUES (1,'김준일','aaa@gmail.com','01012345678'),(2,'이영인','bbb@gmail.com','01051951766'),(3,NULL,NULL,NULL),(4,NULL,NULL,NULL),(5,NULL,NULL,NULL),(6,NULL,NULL,NULL),(7,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `user_dtl` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_mst`
+--
+
 DROP TABLE IF EXISTS `user_mst`;
-CREATE TABLE IF NOT EXISTS `user_mst` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_mst` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- 테이블 데이터 subquery_study.user_mst:~0 rows (대략적) 내보내기
-DELETE FROM `user_mst`;
+--
+-- Dumping data for table `user_mst`
+--
 
--- 트리거 subquery_study.user_mst_after_insert 구조 내보내기
-DROP TRIGGER IF EXISTS `user_mst_after_insert`;
-SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-DELIMITER //
-CREATE TRIGGER `user_mst_after_insert` AFTER INSERT ON `user_mst` FOR EACH ROW BEGIN
-	INSERT into
-		user_dtl
-		(id)
-	values
-		(NEW.id);
-END//
+LOCK TABLES `user_mst` WRITE;
+/*!40000 ALTER TABLE `user_mst` DISABLE KEYS */;
+INSERT INTO `user_mst` VALUES (1,'test'),(2,'test2'),(3,'aaa'),(4,'bbb'),(5,'ccc'),(6,'ddd'),(7,'eee');
+/*!40000 ALTER TABLE `user_mst` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `user_mst_after_insert` AFTER INSERT ON `user_mst` FOR EACH ROW BEGIN
+	INSERT into
+		user_dtl
+		(id)
+	values
+		(NEW.id);
+END */;;
 DELIMITER ;
-SET SQL_MODE=@OLDTMP_SQL_MODE;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `user_mst_BEFORE_DELETE` BEFORE DELETE ON `user_mst` FOR EACH ROW BEGIN
+	delete
+    from
+		user_dtl
+	where
+		id = old.id;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+--
+-- Dumping events for database 'subquery_study'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-14 12:17:17
